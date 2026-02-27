6deb4ada2f7665dca79144c3cf7b2cffa4f80662
Ziad Ahmad <Ziad.Ahmad.intern@linkdevelopment.com>
2026-02-27 05:55:50 +0200

    Added serilog

--- Rahal.Api/Program.cs
+++ Rahal.Api/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Rahal.Api.Extensions;
 using Rahal.Api.Middlewares;
+using Serilog;
 using Shared.Infrastructure;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +11,33 @@ var builder = WebApplication.CreateBuilder(args);
 //Inject services
 builder.Services.AddAllModules(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); //Automatically serialize enums as strings in JSON responses
+    });
+
+
+//Register in HTTP Logging
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+});
+
+//Serilog
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration)
+    =>
+{
+    loggerConfiguration.ReadFrom.Configuration(context.Configuration) //Assigning the project's logging configs to Serilog configs
+    .ReadFrom.Services(services);//Read app services and make them availavle to serilog
+});
+
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.Logger.LogInformation("Application is starting up...");
 
 //Run all pending migrations
 await app.ApplyMigrationsAsync();
